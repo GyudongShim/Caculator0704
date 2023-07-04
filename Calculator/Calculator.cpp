68626ad6d80831d942b49074652c44b5a3a1538a
GyudongShim <gdshim@gmail.com>
2023-07-04 17:11:15 +0900

    First commit for co-work.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+class Calculator
+{
+public:
+	int getSum(int a, int b) { return 0; }
+	int getGop(int a, int b) { return 0; }
+	int getZegop(int a) { return 0; }
+	int getMinus(int a, int b) { return 0; }
+	int getDevide(int a, int b) { return 0; }
+	int getSumSum(int a, int b, int c) { return 0; }
+};
---
+++ CalculatorTest/test.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "../Calculator/Calculator.cpp"
+
+TEST(CalculatorTest, getSum) {
+	Calculator calculator{};
+	int a = 0;
+	int b = 1;
+	auto sum = calculator.getSum(a, b);
+	EXPECT_EQ(sum, a + b);
+}
+
+TEST(CalculatorTest, getGop) {
+	Calculator calculator{};
+	int a = 1;
+	int b = 2;
+	auto multiple = calculator.getGop(a, b);
+	EXPECT_EQ(multiple, a * b);
+}
+
+TEST(CalculatorTest, getZegop) {
+	Calculator calculator{};
+	int a = 2;
+	auto square = calculator.getZegop(2);
+	EXPECT_EQ(square, a*a);
+}
+
+TEST(CalculatorTest, getMinus) {
+	Calculator calculator{};
+	int a = 0;
+	int b = 1;
+	auto delta = calculator.getMinus(a, b);
+	EXPECT_EQ(delta, a - b);
+}
+
+TEST(CalculatorTest, getDevide) {
+	Calculator calculator{};
+	int a = 100;
+	int b = 5;
+	auto devide = calculator.getDevide(a, b);
+	EXPECT_EQ(devide, a / b);
+}
+
+TEST(CalculatorTest, getSumSum) {
+	Calculator calculator{};
+	int a = 0;
+	int b = 1;
+	int c = 2;
+	auto sum = calculator.getSumSum(a, b, c);
+	EXPECT_EQ(sum, a + b + c);
+}
