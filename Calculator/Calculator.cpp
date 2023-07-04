defa7b53c632e4ecaa916fc9f41173ffa754d668
GyudongShim <gdshim@gmail.com>
2023-07-05 08:55:01 +0900

    Implementing GetSumSum and Divide

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -7,6 +7,17 @@ public:
 	int getGop(int a, int b) { return 0; }
 	int getZegop(int a) { return 0; }
 	int getMinus(int a, int b) { return 0; }
-	int getDevide(int a, int b) { return 0; }
-	int getSumSum(int a, int b, int c) { return 0; }
+
+	// return integer divide ( 3 / 2 is 1 not 1 )
+	int getDivide(int a, int b)
+	{
+		if (b == 0)
+		{
+			return 0;
+		}
+
+		return a / b;
+	}
+
+	int getSumSum(int a, int b, int c) { return a + b + c; }
 };
