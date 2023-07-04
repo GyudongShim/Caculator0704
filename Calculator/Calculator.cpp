ca0cc2cf4f4940e190621519b3247edfb2b6c19f
jungi.lee <dffbnm@gmail.com>
2023-07-05 08:58:27 +0900

    getMinus 수정

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -6,7 +6,7 @@ public:
 	int getSum(int a, int b) { return 0; }
 	int getGop(int a, int b) { return 0; }
 	int getZegop(int a) { return 0; }
-	int getMinus(int a, int b) { return 0; }
+	int getMinus(int a, int b) { return a - b; }
 	int getDevide(int a, int b) { return 0; }
 	int getSumSum(int a, int b, int c) { return 0; }
 };
